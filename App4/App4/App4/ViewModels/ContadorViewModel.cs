dd02c2f95844f16e438c0b1af47f0227dfab2af7
Jorge517 <78835423+Jorge517@users.noreply.github.com>
2021-09-27 15:12:09 -0500

    App
    UTEQ

---
+++ App4/App4/App4/ViewModels/ContadorViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace App4.ViewModels
+{
+   public class ContadorViewModel : BaseViewModel
+   {
+        int _cont;
+        ICommand _botonclic;
+        ICommand _restaurarclic;
+        string _contConvertido;
+        public ContadorViewModel()
+        {
+            _cont = 0;
+        }
+
+        public int Contador
+        {
+            get => _cont;
+            set
+            {
+                if (value == _cont) return;
+                _cont = value;
+                ContConvertido = $"Veces presionado: {_cont}";
+                OnPropertyChanged();
+            }
+        }
+        public string ContConvertido
+        {
+            get => _contConvertido;
+            set
+            {
+                if (string.Equals(_contConvertido, value)) return;
+                _contConvertido = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand BotonClic
+        {
+            get
+            {
+                if (_botonclic == null)
+                    _botonclic = new Command(BotonClicAc);
+                return _botonclic;
+            }
+        }
+        public ICommand BotonRestaurar
+        {
+            get
+            {
+                if (_restaurarclic == null)
+                    _restaurarclic = new Command(BotonRestaurarClicAc);
+                return _restaurarclic;
+            }
+        }
+        private void BotonRestaurarClicAc()
+        {
+            Contador = 0;
+            ContConvertido = "Reiniciado";
+        }
+        private void BotonClicAc()
+        {
+            Contador++;
+        }
+    }
+    }
